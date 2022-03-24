4838a077ac6b4dd937c6b7e218e8815d7f605a7b
Ryan Poon <rpoon@mit.edu>
2022-03-24 19:47:28 -0400

    simulation work

--- script/TrajectoryPlanner/DynamicsInteractionSimulation.py
+++ script/TrajectoryPlanner/DynamicsInteractionSimulation.py
@@ -39,97 +39,111 @@ def get_xp_target(x_p, x_a_target, dx_a_target):
 
 # Function: Forward Simulation Step
 # Calculates the passive and active states and derivatives of the next time step
-def forward_sim_step(xp, dxp, xa, dxa, xp_d, dxp_d, xa_d, dx_d):
-    # For robot class function callbacks, state is [x y z yaw pitch roll 4DOF]
-    x = np.concatenate(([0.0, 0.0], xp[0], 0, xp[1], xp[2], xa), axis=None)
-    dx = np.concatenate(([0.0, 0.0], dxp[0], 0, dxp[1], dxp[2], dxa), axis=None)
-    robot.setConfig(x)
-    robot.setVelocity(dx)
+# (x_init, dx_init, a_hat_init, da_hat_init, x_d, dx_d)
+
+def forward_sim_step(x, dx, a_hat, da_hat, x_d, dx_d, dt):
+    # State is [x y z yaw pitch roll 4DOF]
+    x_config = np.concatenate(([0.0, 0.0], x[0], 0, x[1:], 0), axis=None)
+    dx_config = np.concatenate(([0.0, 0.0], dx[0], 0, dx[1:], 0), axis=None)
+
+    robot.setConfig(x_config)
+    robot.setVelocity(dx_config)
 
     # Set PID gain matrices
     alpha = 0.5
-    P, I, D = 59, 20, 13.5
+    P, I, D = 59.0, 20.0, 13.5
     KP = np.asarray([[P, 0, 0], [0, P, 0], [0, 0, P]])
-    KD = np.asarray([[D, 0, 0], [0, D, 0], [0, 0, D]])
     KI = np.asarray([[I, 0, 0], [0, I, 0], [0, 0, I]])
+    KD = np.asarray([[D, 0, 0], [0, D, 0], [0, 0, D]])
 
     # Collect H, G, C matrices and vectors. Resize for calculations
     zero_mat = np.zeros((3,3))
 
     H_mat = np.asarray(robot.getMassMatrix())
-    H_mat = np.delete(H_mat, [0, 1, 3, len(x)-1], 0)
-    H_mat = np.delete(H_mat, [0, 1, 3, len(x)-1], 1)
+    H_mat = np.delete(H_mat, [0, 1, 3, 9], 0)
+    H_mat = np.delete(H_mat, [0, 1, 3, 9], 1)
     g_vec = np.asarray(robot.getGravityForces((0, 0, -9.81)))
-    g_vec = np.delete(g_vec, [0, 1, 3, len(x)-1])
+    g_vec = np.delete(g_vec, [0, 1, 3, 9])
     c_vec = np.asarray(robot.getCoriolisForces())
-    c_vec = np.delete(c_vec, [0, 1, 3, len(x)-1])
+    c_vec = np.delete(c_vec, [0, 1, 3, 9])
 
     # Create K matrix
-    Ks_mat = np.asarray(sus.GetStiffnessMatrix(z=xp[0], qPitch=xp[1], qRoll=xp[2]))
+    Ks_mat = np.asarray(sus.GetStiffnessMatrix(z=x[0], qPitch=x[1], qRoll=x[2]))
     K_top = np.concatenate((Ks_mat, zero_mat), axis=1)
     K_bottom = np.concatenate((zero_mat, np.add(KP, -(1/alpha)*KI)), axis=1)
     K = np.concatenate((K_top, K_bottom), axis=0)
 
     # Create B matrix
-    Kb_mat = np.asarray(sus.GetDampingMatrix(z=xp[0], qPitch=xp[1], qRoll=xp[2]))
+    Kb_mat = np.asarray(sus.GetDampingMatrix(z=x[0], qPitch=x[1], qRoll=x[2]))
     B_top = np.concatenate((Kb_mat, zero_mat), axis=1)
     B_bottom = np.concatenate((zero_mat, KD), axis=1)
     B = np.concatenate((B_top, B_bottom), axis=0)
 
     # Create I vector
+    I_top = -np.matmul(Ks_mat, x_d[:3])
+    I_bottom = np.matmul(KI, a_hat)
+    I = np.concatenate((I_top, I_bottom), axis=0)
 
-    I_top = -np.dot(Ks_mat, xp_d)
-
-
-    quit()
+    # Calculate error vectors
+    x_err = x - x_d
 
     # Right hand side of dynamics equation (without inertial matrix inverse)
-    rhs = -(Cp_vec + np.matmul(Kb_mat, dxp) + np.matmul(Ks_mat, xp) + Gp_vec + np.matmul(H12_mat, ddxa))
-
-    (ddxp, _, _, _) = np.linalg.lstsq(H11_mat, rhs, rcond=None)
-
-    quit()
+    rhs = -c_vec - g_vec - np.matmul(K, x_err) - np.matmul(B, dx) + I
+    (ddx, _, _, _) = np.linalg.lstsq(H_mat, rhs, rcond=None)
 
     # Kinematics calculations
-    dxp_next = dxp + ddxp * self.dt
-    dxa_next = dxa + ddxa * self.dt
-    xp_next = xp + dxp * self.dt + 0.5 * ddxp * self.dt ** 2
-    xa_next = xa + dxa * self.dt + 0.5 * ddxa * self.dt ** 2
+    dx_next = dx + ddx*dt
+    x_next = x + dx*dt + 0.5*ddx*(dt**2)
 
-    return (xp_next, dxp_next, xa_next, dxa_next)
+    # Calculate a_hat_dot and a_hat
+    da_hat_next = -x_err[3:] - (1/alpha)*dx[3:]
+    a_hat_next = a_hat + da_hat_next*dt
+    # print("da: " + str(da_hat_next*dt))
+    # print("a: " + str(a_hat_next))
+
+    return x_next, dx_next, a_hat_next, da_hat_next
 
 
 # Function: Forward Simulation
 # Returns full list of passive and active states, derivatives, and errors for plotting given control input.
-def forward_sim(self, u):
+def simulate(x_init, dx_init, a_hat_init, da_hat_init, x_d, dx_d, dt):
     # Initializing passive and active states and derivatives
-    xp = np.zeros((self.n_steps, self.n_p))
-    xa = np.zeros((self.n_steps, self.n_a))
-    dxp = np.zeros((self.n_steps, self.n_p))
-    dxa = np.zeros((self.n_steps, self.n_a))
-    ddxa = u.reshape((self.n_steps, self.n_a))
+    test_length = 20 # in seconds
+    t = np.asarray([n * dt for n in range(0, int(test_length/dt)+1)])
 
-    delta_xp = np.zeros((self.n_steps, self.n_p))
-    delta_xa = np.zeros((self.n_steps, self.n_a))
+    x = np.asarray([x_init])
+    dx = np.asarray([dx_init])
+    a = np.asarray([a_hat_init])
+    da = np.asarray([da_hat_init])
 
-    # Assigning initial conditions
-    xp[0, :] = self.xp_init
-    xa[0, :] = self.xa_init
-    dxp[0, :] = self.dxp_init
-    dxa[0, :] = self.dxa_init
 
-    delta_xp[0, :] = self.xp_init - self.xp_target
-    delta_xa[0, :] = self.xa_init - self.xa_target
+    for k in range(np.size(t)):
+        # Calculate new states
+        x_new, dx_new, a_new, da_new = forward_sim_step(x[k], dx[k], a[k], da[k], x_d, dx_d, dt)
+        # print(x_new)
 
-    # Forward simulation step
-    for ii in range(self.n_steps - 1):
-        xp[ii + 1, :], dxp[ii + 1, :], xa[ii + 1, :], dxa[ii + 1, :] = self.forward_sim_step(xp[ii, :], dxp[ii, :],
-                                                                                            xa[ii, :], dxa[ii, :],
-                                                                                            ddxa[ii, :])
-        delta_xp[ii + 1, :] = xp[ii + 1, :] - self.xp_target
-        delta_xa[ii + 1, :] = xa[ii + 1, :] - self.xa_target
+        # Add to arrays
+        x = np.append(x, [x_new], axis=0)
+        dx = np.append(dx, [dx_new], axis=0)
+        a = np.append(a, [a_new], axis=0)
+        da = np.append(da, [da_new], axis=0)
+        # print(x)
 
-    return xp, xa, dxp, dxa, delta_xp, delta_xa
+
+    x_target = np.concatenate(([0.0, 0.0], x_d[0], 0, x_d[1:], 0), axis=None)
+    robot.setConfig(x_target)
+    g_v = np.asarray(robot.getGravityForces((0, 0, -9.81)))
+    KI = np.asarray([[20, 0, 0], [0, 20, 0], [0, 0, 20]])
+    KI_inv = np.linalg.inv(KI)
+    print(np.matmul(KI_inv, g_v[6:9]))
+
+    # print(x[-1])
+
+    motor_pos = x[1:, 4]
+    plt.figure(1)
+    plt.plot(t, motor_pos)
+    plt.grid()
+    plt.show()
 
 
 # Function: Main
@@ -146,16 +160,20 @@ if __name__ == "__main__":
     # Initial states
     # Note: In realtime implementation, these are updated to current state whenever the trajectory planner is calleds
     # The state is [z pitch roll 4DOF]
-    xa_init = np.array([0.0] * robot.numDrivers())
-    dxa_init = np.zeros((robot.numDrivers()))
-    xp_init = np.zeros((3)) # xp_target[:]
-    dxp_init = np.zeros((3))
+    x_init = np.zeros((6))
+    dx_init = np.zeros((6))
+    a_hat_init = np.zeros((3))
+    da_hat_init = np.zeros((3))
 
-    xa_d = [1.0472, -2.0, 1.0]
-    dxa_d = [0, 0, 0]
-    xp_d = fmin(get_xp_target, xp_init, args=(xa_d, dxa_d), xtol=0.000001, disp=False)
-    dxp_d = [0, 0, 0]
+    dx_d = np.zeros((6))
+    # xa_d = [1.0472, -2.0, 1.0]
+    xa_d =[1.0472, -np.pi/4, 0.0]
+    xp_d = fmin(get_xp_target, x_init[:3], args=(xa_d, dx_d[:3]), xtol=0.000001, disp=False)
+    x_d = np.concatenate((xp_d, xa_d), axis=0)
 
-    forward_sim_step(xp_init, dxp_init, xa_init, dxa_init, xp_d, dxp_d, xa_d, dxa_d)
+    dt = 0.001
+
+    simulate(x_init, dx_init, a_hat_init, da_hat_init, x_d, dx_d, dt)
+
 
     print(robot.numDrivers())
